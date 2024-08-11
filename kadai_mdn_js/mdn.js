8cd6909ee912122229a245db67ea3269adb6acf4
machi6879 <haruna6399@gmail.com>
2024-08-11 10:31:55 +0900

    課題mds_jsをアップロード

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,6 @@
+const today = new Date();
+const year = today.getFullYear();
+const month = today.getMonth() + 1;
+const dayOfMonth = today.getDate();
+
+console.log(year + '年', month + '月', dayOfMonth + '日');
